1ca77b86090cb6f53ed13668c2658d6c6a2796ad
Anton Stecyura <grandwar39@gmail.com>
2022-09-12 16:25:39 +0300

    fill main page

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Chat from '../components/Chat';
+import { useTypedSelector } from '../hooks/redux';
+import { AiOutlineWarning } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import Button from '../components/Button';
 
 const MainPage = () => {
-  return (
-    <div>
-      <div className='font-title'>Title font QWERTY</div>
-      <div className='font-content'>Content font QWERTY</div>
-    </div>
-  )
-}
+  const { chats } = useTypedSelector((state) => state.chat);
 
-export default MainPage
+  useEffect(() => {
+    // fetchChats()
+  });
+
+  return (
+    <div
+      className={`pt-10 ${
+        !chats ? 'flex items-center justify-center h-full' : ''
+      }`}
+    >
+      {/* chat list */}
+      <div className='flex flex-col gap-10'>
+        {chats ? (
+          chats.map((chat) => (
+            <Button>
+              <Chat
+                key={chat.id}
+                chat={chat}
+              />
+            </Button>
+          ))
+        ) : (
+          <div className='-translate-y-1/2 '>
+            <div className='mb-5 text-center font-title text-neutral text-[3rem] bg-hot-200 w-full py-2 px-4'>
+              No chats started yet
+            </div>
+
+            <Button>
+              <Link
+                to={'/search'}
+                className='text-center text-hot-100 text-xl h-[50px] flex items-center justify-center font-title duration-500 hover:text-cold-300'
+              >
+                Start chatting
+              </Link>
+            </Button>
+
+            <div></div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
