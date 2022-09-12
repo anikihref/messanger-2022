28891612c60767b4455953f24edf254087e0ff2f
Anton Stecyura <grandwar39@gmail.com>
2022-09-12 16:24:29 +0300

    create button and chat components

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const Button = () => {
+interface ButtonProps {
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({children}) => {
   return (
-    <div>Button</div>
+    <button className='bg-cold-300 relative z-[1] w-full
+    before:-z-[1] before:content-[""] before:right-0 before:top-0 before:bottom-0
+    before:bg-hot-100 before:absolute before:w-[20%] before:duration-500 hover:before:w-full'>
+      {children}
+    </button>
   )
 }
 
--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom';
+import { useTypedSelector } from '../hooks/redux';
+import { IChat } from '../types/chat'
+import { IUser } from '../types/user';
 
-const Chat = () => {
+interface ChatProps {
+  chat: IChat;
+}
+
+const Chat: React.FC<ChatProps> = ({ chat }) => {
+  const [chatAvatar, setChatAvatar] = useState<string>('http://localhost:5000/static/empty_avatar.png');
+  const [secondMember, setSecondMember] = useState<IUser>();
+  const {user} = useTypedSelector(state => state.user)
+
+  useEffect(() => {
+    if(chat.members && chat.members.length <= 2) {
+    
+    }
+  })
+
   return (
-    <div>Chat</div>
+    <Link 
+      to={`/chat/${chat.id}`}
+      className='flex p-5  border-[3px] border-cold-200'
+    >
+      {/* avatar */}
+      <div className='rounded-full w-[5%] aspect-square overflow-hidden'>
+        {secondMember ? (
+          <Link to={`/user/${secondMember.id}`} >
+            <img className='min-w-full min-h-full' src={chatAvatar} alt="avatar" />
+          </Link>
+        ) : (
+        <div>
+          <img className='min-w-full min-h-full' src={chatAvatar} alt="avatar" />
+        </div>
+        )}
+      </div>
+      
+
+      <div className='ml-5'>
+        {/* title */}
+        <h5 className='font-title font-[700] mb-1'>{chat.title}</h5>
+        {/* last message */}
+        <div className='font-content text-left'>{chat.lastMessage}</div>
+      </div>
+
+      {/* options */}
+      <div></div>
+    </Link>
   )
 }
 
