e7d93adbd0cffa89da9f8a404cc112190dbafc28
Anton Stecyura <grandwar39@gmail.com>
2022-09-18 14:06:22 +0300

    add chatSlice async/sync actions

--- client/src/store/slices/chatSlice.ts
+++ client/src/store/slices/chatSlice.ts
@@ -1,13 +1,19 @@
+import { fetchChats } from './../actions/fetchChats';
+import { MongooseIDType } from './../../types/index';
 import { IChat } from './../../types/chat';
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface InitialStateProps {
-    chats: IChat[] | null,
+    chats: IChat[],
+    error: string,
+    isLoading: boolean,
     selectedChat: IChat | null
 }
 
 const initialState: InitialStateProps = {
-    chats: null,
+    chats: [],
+    isLoading: false,
+    error: '',
     selectedChat: null
 };
 
@@ -15,6 +21,43 @@ export const chatSlice = createSlice({
     initialState,
     name: 'chat',
     reducers: {
-        // todo: fetch, leave, delete, addUser, removeUser, clearHistory
+        delete(state, action: PayloadAction<MongooseIDType>) {
+            state.chats = state.chats.filter(chat => chat.id !== action.payload)
+        },
+
+        addUser(state, action: PayloadAction<{chatId: MongooseIDType; userId: MongooseIDType}>) {
+            const chat = state.chats.find(chat => chat.id === action.payload.chatId);
+
+            chat?.members.push(action.payload.userId);
+        },
+
+        removeUser(state, action: PayloadAction<{chatId: MongooseIDType; userId: MongooseIDType}>) {
+            const chat = state.chats.find(chat => chat.id === action.payload.chatId);
+            if (!chat) return;
+            chat.members = chat.members.filter(userId => userId !== action.payload.userId)
+        },
+
+        changeTitle(state, action: PayloadAction<{chatId: MongooseIDType; title: string}>) {
+            const chat = state.chats.find(chat => chat.id === action.payload.chatId);
+            if (!chat) return;
+            chat.title = action.payload.title;
+        },
+        select(state, action: PayloadAction<IChat>) {
+            state.selectedChat = action.payload;
+        }
+    },
+    extraReducers: {
+        [fetchChats.fulfilled.type]: (state, action: PayloadAction<IChat[]>) => {
+            state.isLoading = false;
+            state.error = '';
+            state.chats = action.payload;
+        },
+        [fetchChats.rejected.type]: (state, action: PayloadAction<string>) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+        [fetchChats.pending.type]: (state) => {
+            state.isLoading = true;
+        }
     }
 });
