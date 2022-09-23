bd79856bb5246fc0f69b12cd26994ff80a87aea8
Anton Stecyura <grandwar39@gmail.com>
2022-09-23 21:50:48 +0300

    chats are displaying in default layout

--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,24 +1,49 @@
 import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import Button from '../components/Button';
+import Chat from '../components/Chat';
 import Navbar from '../components/Navbar';
+import { useTypedSelector } from '../hooks/redux';
 import useTimeout from '../hooks/useTimeout';
 
 const MainLayout = () => {
-  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
   const {clear, reset} = useTimeout(() => setSidebarOpen(false) , 1000);
+  const {chats} = useTypedSelector(state => state.chat)
 
   useEffect(() => {
-    clear()
+
   })
 
   return (
     <div className='h-full  bg-neutral'>
-      <div className='container h-full'>
-        <Outlet />
+      <div className='container max-h-[90%] flex'>
+        {/* Chatlist */}
+        <div className='w-[40%] p-5 flex flex-col gap-y-2 overflow-y-scroll'>
+        {chats.toString() ? chats.map((chat) => (
+            <Button
+            key={chat.id}>
+              <Chat
+                
+                chat={chat}
+              />
+            </Button>
+          )) : <div>
+              no chats yet
+            </div>}
+        </div>
 
+
+          {/* Custom content */}
+          <div className='grow overflow-auto p-2'>
+            <Outlet />  
+          </div>
+
+
+        </div>
         {/* sidebar trigger */}
         <div
-          className='fixed bottom-0 h-[10%] left-0 right-0'
+          className='fixed bottom-0 h-[10%] left-0 right-0 z-[100]'
           onMouseOver={() => {
             clear()
             setSidebarOpen(true)
@@ -31,7 +56,6 @@ const MainLayout = () => {
           <div className="container absolute opacity-50 bottom-[20%] h-[20px] left-5 right-5 bg-cold-300 rounded-xl"></div>
           <Navbar isOpened={sidebarOpen}></Navbar>
         </div>
-      </div>
     </div>
   );
 };
--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,53 +1,9 @@
-import React, { useEffect } from 'react';
-import Chat from '../components/Chat';
-import { useTypedSelector } from '../hooks/redux';
-import { AiOutlineWarning } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
-import Button from '../components/Button';
+import React from 'react';
 
 const MainPage = () => {
-  const { chats } = useTypedSelector((state) => state.chat);
-
-  useEffect(() => {
-    // fetchChats()
-  });
-
   return (
-    <div
-      className={`pt-10 ${
-        !chats ? 'flex items-center justify-center h-full' : ''
-      }`}
-    >
-      {/* chat list */}
-      <div className='flex flex-col gap-10'>
-        {chats ? (
-          chats.map((chat) => (
-            <Button>
-              <Chat
-                key={chat.id}
-                chat={chat}
-              />
-            </Button>
-          ))
-        ) : (
-          <div className='-translate-y-1/2 '>
-            <div className='mb-5 text-center font-title text-neutral text-[3rem] bg-hot-200 w-full py-2 px-4'>
-              No chats started yet
-            </div>
-
-            <Button>
-              <Link
-                to={'/search'}
-                className='text-center text-hot-100 text-xl h-[50px] flex items-center justify-center font-title duration-500 hover:text-cold-300'
-              >
-                Start chatting
-              </Link>
-            </Button>
-
-            <div></div>
-          </div>
-        )}
-      </div>
+    <div>
+        asd
     </div>
   );
 };
