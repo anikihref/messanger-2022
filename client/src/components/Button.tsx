133023ed82a22b7c33f3a62ff5f9da7fe7fb2bd9
Anton Stecyura <grandwar39@gmail.com>
2022-09-25 16:16:34 +0300

    add load more button and add chats limit state

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({children}) => {
   return (
-    <button className='bg-cold-300 relative z-[1] w-full
+    <button className='bg-cold-300 relative z-[1] w-full duration-[900ms] hover:text-cold-300 text-hot-100
     before:-z-[1] before:content-[""] before:right-0 before:top-0 before:bottom-0
     before:bg-hot-100 before:absolute before:w-[20%] before:duration-500 hover:before:w-full'>
       {children}
--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,50 +1,62 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Button from '../components/Button';
 import Chat from '../components/Chat';
 import NavbarTrigger from '../components/NavbarTrigger';
 import { useTypedDispatch, useTypedSelector } from '../hooks/redux';
 import { fetchChats } from '../store/actions/fetchChats';
+import {AiOutlineCloudDownload} from 'react-icons/ai'
 
 const MainLayout = () => {
   const dispatch = useTypedDispatch();
-  const {chats} = useTypedSelector(state => state.chat)
-  const {user} = useTypedSelector(state => state.user)
+  const { chats } = useTypedSelector((state) => state.chat);
+  const { user } = useTypedSelector((state) => state.user);
+  const [chatsLimit, setChatsLimit] = useState<number>(20);
 
   useEffect(() => {
     if (user) {
-      dispatch(fetchChats(user.id))
+      dispatch(fetchChats({ userId: user.id, limit: chatsLimit }));
     }
-  }, [user])
+  }, [user, chatsLimit]);
 
   return (
     <div className='h-full  bg-neutral'>
       <div className='container max-h-[90%] flex'>
-
         {/* Chatlist */}
         <div className='w-[40%] p-5 flex flex-col gap-y-2 overflow-y-auto'>
-          {chats.toString() ? chats.map((chat) => (
-              <Button
-              key={chat.id}>
-                <Chat
-                  
-                  chat={chat}
-                />
+          {chats.toString() ? (
+            chats.map((chat) => (
+              <Button key={chat.id}>
+                <Chat chat={chat} />
               </Button>
-            )) : <div>
-              no chats yet
-          </div>}
+            ))
+          ) : (
+            <div>no chats yet</div>
+          )}
+
+            {/* load more button */}
+            <div className='w-1/2 min-w-[50px] mx-auto mt-5 '>
+              <Button>
+                <div className='border-cold-200 border-2 flex items-center px-2'>
+                  <div className='h-[50px] flex items-center tracking-wide text-lg'>Load more</div>
+                  <div className='w-[2rem] ml-3'>
+                    <AiOutlineCloudDownload size={'100%'}/>
+                  </div>
+                </div>
+                
+              </Button>
+            </div>
+            
         </div>
 
-
-          {/* Custom content */}
-          <div className='grow overflow-y-auto p-2'>
-            <Outlet />  
-          </div>
+        {/* Custom content */}
+        <div className='grow overflow-y-auto p-2'>
+          <Outlet />
         </div>
+      </div>
 
-        {/* navbar trigger */}
-        <NavbarTrigger />
+      {/* navbar trigger */}
+      <NavbarTrigger />
     </div>
   );
 };
