8cea421a98a7e30c618df62dc316f2bf5de24ccb
Anton Stecyura <grandwar39@gmail.com>
2022-09-25 14:25:38 +0300

    create api methods and fix userDto import path

--- client/src/api/messageApi.ts
+++ client/src/api/messageApi.ts
@@ -1,6 +1,40 @@
+import { MongooseIDType } from './../types/index';
 import { URLs, baseURL } from './index';
-import axios from "axios";
+import axios, {AxiosResponse} from "axios";
+import { IChatMessage } from '../types/chatMessage';
 
-export const messageApi = axios.create({
+export const baseMessageUrl = axios.create({
     baseURL: baseURL + URLs.Message
 })
+
+export const messageApi = {
+    async getMessage(messageId: MongooseIDType): Promise<AxiosResponse<IChatMessage, any>> {
+        return await baseMessageUrl.get<IChatMessage>(`/${messageId}`)
+    },
+
+    async getAllChatMessages(chatId: MongooseIDType, limit?: number): Promise<AxiosResponse<IChatMessage[], any>> {
+        return await baseMessageUrl.get<IChatMessage[]>(`/all/${chatId}?limit=${limit}`)
+    },
+
+    async deleteAllMessages(chatId: MongooseIDType): Promise<void> {
+        await baseMessageUrl.delete(`/all/${chatId}`);
+    },
+
+    async deleteMessage(messageId: MongooseIDType): Promise<void>  {
+        await baseMessageUrl.delete(`/${messageId}`);
+    },
+
+    async createMessage(message: Omit<IChatMessage, 'createdAt' | 'updatedAt' | 'id'>): Promise<AxiosResponse<IChatMessage, any>> {
+        return await baseMessageUrl.post<IChatMessage>('/', {
+            data: message
+        })
+    },
+
+    async editMessage(messageId: MongooseIDType, value: string): Promise<AxiosResponse<IChatMessage, any>> {
+        return await baseMessageUrl.patch<IChatMessage>(`/${messageId}`, {
+            data: {
+                message: value,
+            }
+        })
+    }
+}
