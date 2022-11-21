96e3aa8e5818a309aa30d64f45ff0e80e6b55d7a
Anton Stecyura <grandwar39@gmail.com>
2022-11-21 22:25:46 +0200

    Add awaiting feature for WebSocket actions
    Now sending, leaving and joining the room can be awaited.
    In client/src/websockets/messageWS.ts create handler function that improves websockets connection by awaiting for the websocket response.
    It helps more precisely check if response came to another user.
    On server ws.js send message also to user that emited to let sender understand if response came to another user

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,5 +1,3 @@
-import { IChatMessage } from "./chatMessage";
-
 export type IDType = string;
 export type ImageType = string;
 export type MongooseIDType = string;
@@ -7,10 +5,4 @@ export type JSONString = string;
 export type TailwindClass = string;
 export type Error = any;
 
-export interface WSMessage {
-    type: 'message' | 'connectionMessage' | 'closeMessage' | 'changeRoom';
-    roomId?: MongooseIDType; // chat id
-    connectionId: MongooseIDType; // user id
-    message?: IChatMessage;
-}
   
--- client/src/websockets/messageWS.ts
+++ client/src/websockets/messageWS.ts
@@ -1,46 +1,142 @@
-import { WebSocketCloseMessage, WebSocketMessage, WebSocketConnectionMessage } from '../types/ws/wsMessages';
+import { WSSuccessResponse, WSErrorResponse } from './../types/ws/index';
+import { WebSocketLeaveMessage, WebSocketJoinMessage, EVENT_TYPES, WebSocketSendMessage } from '../types/ws/messageWS';
+import { MongooseIDType } from '../types';
+
 export interface IMessageWebSocket {
     webSocket: WebSocket | null;
+    handler: (evt: MessageEvent) => any;
+    connectionId?: MongooseIDType;
 }
+
+type SendType<T> = T & {type: EVENT_TYPES.join | EVENT_TYPES.leave | EVENT_TYPES.send} 
 
 class MessageWebSocket implements IMessageWebSocket {
     webSocket: WebSocket | null;
+    handler: (evt: MessageEvent) => void;
+    connectionId: MongooseIDType;
 
     constructor() { 
         this.webSocket = null;
+        this.handler = () => null;
+        this.connectionId = '';
     }
 
-    connect(message: WebSocketConnectionMessage, onMessage: (arg: any) => void) {
-        this.webSocket = new WebSocket('ws://localhost:8000');
+    handleResponse<T extends WSSuccessResponse>(
+        eventType: EVENT_TYPES,
+    ): Promise<T> {
+        let resolveScoped: (value: T | PromiseLike<T>) => void;
+        let rejectScoped: (reason?: any) => void; 
 
-        this.webSocket.onopen = () => {
-            this.webSocket?.send(JSON.stringify(message))
+        function responseHandler (
+            evt: MessageEvent<any>
+        ) {
+            const response: T = JSON.parse(evt.data);
+            if (!resolveScoped || !rejectScoped) return; 
+            
+            if (response.eventType === eventType) {
+                if (response.responseState === 'success') {
+                    resolveScoped(response)
+                } else {
+                    rejectScoped(response)
+                }
+            }
+        }
+    
+        const promise: Promise<T> = new Promise((resolve, reject) => {
+            resolveScoped = resolve
+            rejectScoped = reject
+    
+            this.webSocket?.addEventListener('message', responseHandler)
+        })
+    
+        promise
+            .then(() => {
+                console.log(eventType)
+            })
+            .catch((error: WSErrorResponse) => {
+                console.error(`\nMessage WS error in event: ${error.eventType} \nError Message: ${error.message}`)
+            })
+            .finally(() => {
+                this.webSocket?.removeEventListener('message', responseHandler)
+            })
+
+
+        return promise
+    }
+
+    connect(url: string = 'ws://localhost:8000', userId: MongooseIDType) {
+        this.webSocket = new WebSocket(url);
+        this.connectionId = userId
+
+        // window.addEventListener('beforeunload', () => {
+        //     this.leaveRoom({
+        //         connectionId: this.connectionId
+        //     })
+
+        //     this.disconnect();
+        // })
+    }
+
+    async disconnect(): Promise<boolean> {
+        if (this.webSocket?.readyState !== 1 || !this.webSocket) return false;
+
+        this.webSocket.close();
+
+        const promise: Promise<boolean> = new Promise((resolve, reject) => {
+            if (!this.webSocket) return;
+
+            setTimeout(() => {
+                reject('Timed out! Could disconnect from WebSokcet Server')
+            }, 500)
+
+            this.webSocket.onclose = () => {
+                this.webSocket = null;
+                resolve(true)
+            }
+        })
+
+        // websocket will only exist if promise rejected
+        promise.catch(() => this.webSocket!.onclose = () => {})
+
+        return promise;
+    }
+
+    async joinRoom<T extends WebSocketJoinMessage>(message: T): Promise<WSSuccessResponse> {
+        const JSONMessage: SendType<T> = {
+            ...message,
+            type: EVENT_TYPES.join,
         }
 
-        this.webSocket.onmessage = onMessage;
+        this.webSocket?.send(JSON.stringify(JSONMessage))
+
+        this.webSocket?.addEventListener('message', this.handler)
+        
+        return this.handleResponse<WSSuccessResponse>(EVENT_TYPES.join);
     }
 
-    close(message: WebSocketCloseMessage) {
-        this.webSocket?.send(JSON.stringify(message))
-        this.webSocket?.close()
-
-        if (!this.webSocket) { return }
-
-        this.webSocket.onclose = () => {
-            this.webSocket = null;
+    async leaveRoom(message: WebSocketLeaveMessage): Promise<WSSuccessResponse> {
+        const JSONMessage: SendType<WebSocketLeaveMessage> = {
+            ...message,
+            type: EVENT_TYPES.leave,
         }
+
+        this.webSocket?.send(JSON.stringify(JSONMessage))
+
+        this.webSocket?.removeEventListener('message', this.handler)
+
+        return this.handleResponse<WSSuccessResponse>(EVENT_TYPES.leave)
     }
 
-    send(message: WebSocketMessage) {
-        const sendMessage = () => {
-            this.webSocket?.send(JSON.stringify(message))
+    // returns promise (.then => if message was delivered to the receiver) (.catch => if not)
+    async send<T extends WebSocketSendMessage>(message: T): Promise<WSSuccessResponse> {
+        const JSONMessage: SendType<T> = {
+            ...message,
+            type: EVENT_TYPES.send
         }
         
-        if (this.webSocket?.readyState === 0) {
-            return setTimeout(sendMessage, 200);
-        }
-
-        sendMessage();   
+        this.webSocket?.send(JSON.stringify(JSONMessage))
+        
+        return this.handleResponse<WSSuccessResponse>(EVENT_TYPES.send);
     }
 }
 
